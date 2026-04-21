cbea15770078c5d6d604b9c52763997d866cd04d
yamisora-coder <hoangnk2003@gmail.com>
2026-04-21 21:10:26 +0700

    validation
    again

--- BE-feat-entity-full/src/main/java/sp26/group/busticket/modules/repository/CoachRepository.java
+++
@@ -1,9 +0,0 @@
-package sp26.group.busticket.modules.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import sp26.group.busticket.modules.entity.CoachEntity;
-
-@Repository
-public interface CoachRepository extends JpaRepository<CoachEntity, Integer> {
-}
